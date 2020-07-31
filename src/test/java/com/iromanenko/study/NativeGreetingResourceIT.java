063903f19c39855fd6b5d8ffc7b76cd97caeddff
iryna.romanenko <ni.rinka@gmail.com>
2020-07-31 14:25:50 +0100

    Initialising quarkus project

---
+++ src/test/java/com/iromanenko/study/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.iromanenko.study;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
